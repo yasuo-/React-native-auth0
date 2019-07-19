cb51c61576828b5c6acf3ba425c9c6f5e7262dd3
yasuo- <breitling1216@gmail.com>
2019-07-20 07:56:13 +0900

    Add: StatusBar

--- rnapp/src/App.js
+++ rnapp/src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-
-import { mapping, light as lightTheme } from '@eva-design/eva';
-import { ApplicationProvider } from 'react-native-ui-kitten';
+import React, {Component} from 'react';
+import {StatusBar, StyleSheet, View, UIManager} from 'react-native';
+import {mapping, light as lightTheme} from '@eva-design/eva';
+import {ApplicationProvider} from 'react-native-ui-kitten';
 
 import WelcomeScreen from './screens/welcome/WelcomeScreen';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { accessToken: null };
+    this.state = {accessToken: null, isLoadingComplete: false};
   }
 
   render() {
     return (
       <ApplicationProvider mapping={mapping} theme={lightTheme}>
+        <StatusBar barStyle="light-content" backgroundColor={'#000000'} />
         <WelcomeScreen />
       </ApplicationProvider>
     );
